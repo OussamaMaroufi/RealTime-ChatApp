33cbadb89ebdbb2be1726ac595eeeeeffe5c6ce4
OussamaMaroufi <oussama.maaroufi.ing@gmail.com>
2022-01-31 14:54:38 +0100

    Update Room Model

--- chat/models.py
+++ chat/models.py
@@ -7,6 +7,19 @@ class Room(models.Model):
     name = models.CharField(max_length=128)
     online = models.ManyToManyField(to=User, blank=True)
 
+    def get_online_count(self):
+        return self.online.count()
+
+    def join(self, user):
+        self.online.add(user)
+        self.save()
+
+    def leave(self, user):
+        self.online.remove(user)
+        self.save()
+
+    def __str__(self):
+        return f'{self.name} ({self.get_online_count()})'
 
 
 
