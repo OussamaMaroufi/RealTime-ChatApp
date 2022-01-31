c60c6ce046c2b04adf8179d74bc4d3f6bc7bd3f8
OussamaMaroufi <oussama.maaroufi.ing@gmail.com>
2022-01-31 15:00:19 +0100

    dd index and room views to chat app

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from chat.models import Room
 
-# Create your views here.
+
+def index_view(request):
+    return render(request, 'index.html', {
+        'rooms': Room.objects.all(),
+    })
+
+def room_view(request, room_name):
+
+    # create the if it doesn't exist  
+    chat_room, created = Room.objects.get_or_create(name=room_name)
+    return render(request, 'room.html', {
+        'room': chat_room,
+    })
+ 
