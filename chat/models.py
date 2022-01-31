7d8c500af8da92de20ec5a9cf75a7eb21751685e
OussamaMaroufi <oussama.maaroufi.ing@gmail.com>
2022-01-31 14:53:17 +0100

    Add Room Model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=128)
+    online = models.ManyToManyField(to=User, blank=True)
+
