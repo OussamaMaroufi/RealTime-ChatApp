c53af679ad809f3709808114f19ab4bd146dca0e
OussamaMaroufi <oussama.maaroufi.ing@gmail.com>
2022-01-31 14:53:52 +0100

    Add Message Model

--- chat/models.py
+++ chat/models.py
@@ -7,3 +7,14 @@ class Room(models.Model):
     name = models.CharField(max_length=128)
     online = models.ManyToManyField(to=User, blank=True)
 
+
+
+
+class Message(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
+    content = models.CharField(max_length=512)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content} [{self.timestamp}]'
