6e5c466ee43098b39e945792e5dcd9d78504ca56
OussamaMaroufi <oussama.maaroufi.ing@gmail.com>
2022-02-01 09:23:29 +0100

    Register routing.py inside asgi.py

--- django_channels/asgi.py
+++ django_channels/asgi.py
@@ -12,11 +12,15 @@ import os
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': URLRouter(
+      chat.routing.websocket_urlpatterns
+    ),
 })
 
 
