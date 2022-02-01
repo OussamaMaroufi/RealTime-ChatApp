2a7bc41d04670528824bdb4547431b9ac6943e0c
OussamaMaroufi <oussama.maaroufi.ing@gmail.com>
2022-02-01 13:39:53 +0100

    Handle messages from the frontend

--- chat/static/room.js
+++ chat/static/room.js
@@ -28,14 +28,14 @@ function onlineUsersSelectorRemove(value) {
 chatMessageInput.focus();
 
 // submit if the user presses the enter key
-chatMessageInput.onkeyup = function(e) {
+chatMessageInput.onkeyup = function (e) {
     if (e.keyCode === 13) {  // enter key
         chatMessageSend.click();
     }
 };
 
 // clear the 'chatMessageInput' and forward the message
-chatMessageSend.onclick = function() {
+chatMessageSend.onclick = function () {
     if (chatMessageInput.value.length === 0) return;
 
     //send msg to grp layer 
@@ -53,13 +53,13 @@ let chatSocket = null;
 function connect() {
     chatSocket = new WebSocket("ws://" + window.location.host + "/ws/chat/" + roomName + "/");
 
-    chatSocket.onopen = function(e) {
+    chatSocket.onopen = function (e) {
         console.log("Successfully connected to the WebSocket.");
     }
 
-    chatSocket.onclose = function(e) {
+    chatSocket.onclose = function (e) {
         console.log("WebSocket connection closed unexpectedly. Trying to reconnect in 2s...");
-        setTimeout(function() {
+        setTimeout(function () {
             console.log("Reconnecting...");
             connect();
         }, 2000);
@@ -67,13 +67,32 @@ function connect() {
 
     //this event launched after msg difff from socket to the group 
 
-    chatSocket.onmessage = function(e) {
+    chatSocket.onmessage = function (e) {
         const data = JSON.parse(e.data);
         console.log(data);
 
         switch (data.type) {
+
             case "chat_message":
-                chatLog.value += data.user + ": " + data.message + "\n";                break;
+                chatLog.value += data.user + ": " + data.message + "\n";
+                break;
+            case "user_list":
+                for (let i = 0; i < data.users.length; i++) {
+                    onlineUsersSelectorAdd(data.users[i]);
+                }
+                break;
+            case "user_join":
+                chatLog.value += data.user + " joined the room.\n";
+                onlineUsersSelectorAdd(data.user);
+                break;
+            case "user_leave":
+                chatLog.value += data.user + " left the room.\n";
+                onlineUsersSelectorRemove(data.user);
+                break;
+
+
+
+
             default:
                 console.error("Unknown message type!");
                 break;
@@ -82,7 +101,7 @@ function connect() {
         // scroll 'chatLog' to the bottom
         chatLog.scrollTop = chatLog.scrollHeight;
     };
-    chatSocket.onerror = function(err) {
+    chatSocket.onerror = function (err) {
         console.log("WebSocket encountered an error: " + err.message);
         console.log("Closing the socket.");
         chatSocket.close();
